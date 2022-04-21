ba33c660edc7988516a1d8a3cac2cf45bb14c806
YJ_0_0 <choyongjun1491@gmail.com>
2022-04-21 09:27:14 +0800

    Update lane1.py

--- lane1.py
+++ lane1.py
@@ -6,7 +6,7 @@ def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
     print(image.shape)
     y1 = image.shape[0]
-    y2 = int(y1*(5/3))
+    y2 = int(y1*(3/5))
     x1 = int((y1 - intercept)/slope)
     x2 = int((y2 - intercept)/slope)
     return np.array([x1, y1, x2, y2])
@@ -14,18 +14,19 @@ def make_coordinates(image, line_parameters):
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
-    for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1, y1), (x2, y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-        print("para",parameters)
-        if slope < 0:
-            left_fit.append((slope, intercept))
-        else:
-            right_fit.append((slope, intercept))
-        #print(left_fit)
-        #print(right_fit)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            print(parameters)
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+            #print(left_fit)
+            #print(right_fit)
     left_fit_average = np.average(left_fit, axis=0)
     right_fit_average = np.average(right_fit, axis=0)
     #print(left_fit_average, "left")
@@ -35,7 +36,7 @@ def average_slope_intercept(image, lines):
     return np.array([left_line, right_line])
 
 def canny(image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
@@ -67,8 +68,7 @@ lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineL
 averaged_lines = average_slope_intercept(lane_image, lines)
 line_image = display_lines(lane_image, averaged_lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow("linr", line_image)
 cv2.imshow("combo", combo_image)
 cv2.waitKey(0)
-plt.imshow(combo_image)
-plt.show()
+#plt.imshow(combo_image)
+#plt.show()
